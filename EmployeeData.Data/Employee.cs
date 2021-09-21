ecc314e50d2ed168cc76bd53203d7788491163e6
Adam Woerz <azreow@gmail.com>
2021-09-21 19:54:37 -0400

    Committing branch w/Employee data and some questionable stuff to my own dev branch.

--- EmployeeData.Data/Employee.cs
+++ EmployeeData.Data/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace EmployeeData.Data
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        [ForeignKey(nameof(Location))]
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+
+        [ForeignKey(nameof(Position))]
+        public int PositionId { get; set; }
+        public virtual Position Position { get; set; }
+
+        //Questionable.
+
+        [ForeignKey(nameof(Benefit))]
+        public List<int> Benefits { get; set; }
+        public virtual Benefit Benefit { get; set; }
     }
 }
