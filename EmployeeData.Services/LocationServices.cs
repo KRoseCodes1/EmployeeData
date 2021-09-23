c1a9d776ea09894c1b1954076463f2634358de1d
Brandon Gordon <BGordon9112@gmail.com>
2021-09-23 18:17:16 -0400

    location new

--- EmployeeData.Services/LocationServices.cs
+++ EmployeeData.Services/LocationServices.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Graph;
+﻿using EmployeeData.WebApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EmployeeData.Models;
+using EmployeeData.Data;
 
 namespace EmployeeData.Services
 {
@@ -18,14 +20,43 @@ namespace EmployeeData.Services
     }
     public bool CreateLocation(LocationCreate model)
     {
-        var entity = 
-            new Post()
+        var entity =
+            new Location()
             {
-               LocationName = model.LocationName,
-               Address = model.Address,
-               PhoneNumber = model.PhoneNumber
+                LocationName = model.LocationName,
+                Address = model.Address,
+                PhoneNumber = model.PhoneNumber
+            };
+       
+        using (var ctx = new ApplicationDbContext())
+        {
+            ctx.Locations.Add(entity);
+            return ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<LocationListItem> GetLocations()
+        {
+            using (var ctx = new Application())
+            {
+                var query = 
+                    ctx
+                        .Locations
+                        .Where(else => else.OwnerId == _userId)
+                        .Select(
+                             else =>
+                                  new Location
+                                  {
+                                     LocationName = e.LocationName,
+                                     Address
+                                     PhoneNumber
+
+                                  };
+                                return Query.ToArray();
+                                                                  
             }
+        }
     }
 
 
-}
+
+
