808a0ebfff9d2bb592550eb2750b7072e96f7272
Adam Woerz <azreow@gmail.com>
2021-09-23 20:37:56 -0400

    Added Employee Controller

---
+++ EmployeeData.WebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeData.Models;
+using EmployeeData.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeData.WebApi.Controllers
+{
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+        private EmployeeService CreateEmployeeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var employeeService = new EmployeeService(userId);
+            return employeeService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            EmployeeService employeeService = CreateEmployeeService();
+            var employees = employeeService.GetEmployees();
+            return Ok(employees);
+        }
+
+        public IHttpActionResult Post(EmployeeCreate employee)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateEmployeeService();
+
+            if (!service.CreateEmployee(employee))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
